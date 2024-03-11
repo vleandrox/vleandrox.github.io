037befff3112346d09c100b425c2a70063f85f29
Victor Alarcon <69864824+vleandrox@users.noreply.github.com>
2024-03-10 22:11:29 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "VICTOR";
+
+// let contenidoTitulo = "Victor";
+// let titulo = document.querySelector(".logo")
+// titulo.innerHTML = contenidoTitulo;
+
+// let textoTitulo = titulo.innerText;
+// console.log(textoTitulo)
+
+// if(textoTitulo=="Nombre"){
+//     titulo.innerHTML="Otro";
+// }else{
+//     console.log("No se cumple")
+// }
+
+// let nombre1 = "VICTOR";
+// let ciudad1 = "HUACHO";
+// let gusto1 =  "FULBITO";
+
+// let parrafo = document.querySelector(".parrafo2");
+
+// function CambiarTexto(nombre1,ciudad1,gusto1){
+//     let contenido=`Esta pagina se ${nombre1} con el fin de informar a muchos jovenes y adultos sobre tecnologia. 
+//     Me encantaría que leas y te ${ciudad1} de la actualidad tecnologica en el mundo.
+//    No te olvides que si queires ${gusto1} mas noticias dale en suscribir mas abajo y recibiar las ultimas noticias por correo.`;
+//     return contenido;
+// }
+// parrafo.innerText =CambiarTexto(nombre1,ciudad1,gusto1);
